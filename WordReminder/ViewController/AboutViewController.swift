adbea960550e6e59f0a41c405568b42686bb7728
Burak <burakgomec101@gmail.com>
2021-07-27 23:54:14 +0300

    Add localization(Turkish) files

--- WordReminder/ViewController/AboutViewController.swift
+++ WordReminder/ViewController/AboutViewController.swift
@@ -14,7 +14,7 @@ class AboutViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()        
-        infoTextView.text = "Word Reminder is an open source translation reminder application. \n\nYou can access the entire source code of the application via the link below.\n\nYou can support my development of independent applications and offer suggestions by giving stars to the application on the App Store and Github. \n\nIcons made by www.freepik.com \n\nDeveloper: Burak GÖMEÇ"
+        infoTextView.text = "Word Reminder is an open source translation reminder application. \n\nYou can access the entire source code of the application via the link below.\n\nYou can support my development of independent applications and offer suggestions by giving stars to the application on the App Store and Github. \n\nIcons made by www.freepik.com \n\nDeveloper: Burak GÖMEÇ".localizeString()
     }
 
 }
